becd97582f6d0ee3dd22d999e301a59e9e556fa6
Vlerant Saiti <vlerant1saiti@gmail.com>
2018-12-17 18:08:54 +0100

    Revert "Cunsumption Stats - API"
    This reverts commit 5c924603adc8e4b445b8147910c2e8d4d387310f.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,15 @@
-const express = require("express");
+const express = require('express');
 
-const AuthRoutes = require("./auth");
-const UserRoutes = require("./user");
-const MarinaRoutes = require("./marina");
-const TestRoutes = require("./test");
-const StatsRoute = require("./stats");
+const AuthRoutes = require('./auth');
+const UserRoutes = require('./user');
+const MarinaRoutes = require('./marina');
+const TestRoutes = require('./test');
 
 module.exports = function(app, io, mqtt) {
-  app.use("/api/auth", AuthRoutes);
-  app.use("/api/user", UserRoutes);
-  app.use("/api/test", TestRoutes);
-  app.use("/api/marina", MarinaRoutes);
-  app.use("/api/stats", StatsRoute);
-};
+    app.use('/api/auth', AuthRoutes);
+    app.use('/api/user', UserRoutes);
+    app.use('/api/test', TestRoutes);
+    app.use('/api/marina', MarinaRoutes);
+}
 
 // module.exports = router;
