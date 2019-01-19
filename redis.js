e383913bf72ba687b31fb511e229b6db4e4bb462
vlerants <35700615+vlerants@users.noreply.github.com>
2019-01-19 12:45:36 +0100

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,8 +1,8 @@
-const redis = require('redis');
-const redisClient = redis.createClient('redis://h:p4f6afd93364207bd9e5a98839c1dc4bec6aa13695755293bb0ba790c6af11f98@ec2-34-207-32-74.compute-1.amazonaws.com:19429');
+// const redis = require('redis');
+// const redisClient = redis.createClient('redis://h:p4f6afd93364207bd9e5a98839c1dc4bec6aa13695755293bb0ba790c6af11f98@ec2-34-207-32-74.compute-1.amazonaws.com:19429');
 
-redisClient.on('connect', function (err, response) {
-    console.log("Connected to redis");
-});
+// redisClient.on('connect', function (err, response) {
+//     console.log("Connected to redis");
+// });
 
-module.exports = redisClient;
+// module.exports = redisClient;
